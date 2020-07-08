1911f9f29ad8ebd145dce8cd3f62a3b4c262e4f4
Raja Tayyab <rtayyab1017@gmail.com>
2020-07-08 18:07:02 +0500

    Faked2

--- dbfinal/settings.py
+++ dbfinal/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sightlibrary.herokuapp.com']
+ALLOWED_HOSTS = ['sightlibrary.herokuapp.com',
+                '127.0.0.1']
 
 
 # Application definition
