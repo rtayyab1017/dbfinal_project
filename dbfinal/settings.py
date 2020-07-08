3f786c478994f7065202d6cbe2680cfdc04e4ea3
Raja Tayyab <rtayyab1017@gmail.com>
2020-07-08 17:45:32 +0500

    Change localhost to ip

--- dbfinal/settings.py
+++ dbfinal/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'library',
         'USER': 'SightAdmin',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
